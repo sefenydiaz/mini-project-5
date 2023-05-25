a15c5bb6d773a63442e8034f12cabd98dc205106
LeAnne B <branchleannelmb@gmail.com>
2023-05-25 11:37:28 -0400

    adds in working timer

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -6,10 +6,12 @@ function getTime() {
     var currentTime = (dayjs().format('MMMM DD[,] YYYY [at] HH:mm:ss a'));
 
     //place into current element 
-    currentEL = currentTime; 
+    //currentEL.innerHTML = currentTime; 
     console.log(currentTime);
-    console.log(typeof(currentTime));
-    console.log(currentEL.innerText)
+    //console.log(typeof(currentTime));
+    currentEL.text(currentTime);
+    //console.log(currentEL.text)
 }
+   
 
-setInterval(getTime, 1000)
+setInterval(getTime, 1000);
