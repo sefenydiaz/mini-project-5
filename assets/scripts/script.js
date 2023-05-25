fc8a19a711e0a4d671d22a74f01f0225c83092b6
LeAnne B <branchleannelmb@gmail.com>
2023-05-25 11:07:48 -0400

    adds in code to plug in current time into header element

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,11 +1,13 @@
 //LeAnne waz here 2023
 
+var currentEL = $("#current")//id is current
+
 function getTime() {
-    var currentTime = (dayjs().format('MMMM DD[,] YYYY [at] HH:mm:ss'));
+    var currentTime = (dayjs().format('MMMM DD[,] YYYY [at] HH:mm:ss a'));
+
+    //place into current element 
+    currentEL.textContent = currentTime; 
     console.log(currentTime);
-
 }
-
-
 
 setInterval(getTime, 1000)
