fa59bfaa72cb41d0fb80d43198d2d49b1e248ba3
LeAnne B <branchleannelmb@gmail.com>
2023-05-25 10:52:48 -0400

    adds in setInterval to call getTime function to get current time every second

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,2 +1,11 @@
 //LeAnne waz here 2023
 
+function getTime() {
+    var currentTime = (dayjs().format('MMMM DD[,] YYYY [at] HH:mm:ss'));
+    console.log(currentTime);
+
+}
+
+
+
+setInterval(getTime, 1000)
