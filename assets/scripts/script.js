872e35502e26e252f34bfb989029c31e461eba08
LeAnne B <branchleannelmb@gmail.com>
2023-05-25 11:25:35 -0400

    adds in timer function

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,13 +1,15 @@
 //LeAnne waz here 2023
 
-var currentEL = $("#current")//id is current
+var currentEL = $("#current"); //id is current
 
 function getTime() {
     var currentTime = (dayjs().format('MMMM DD[,] YYYY [at] HH:mm:ss a'));
 
     //place into current element 
-    currentEL.textContent = currentTime; 
+    currentEL = currentTime; 
     console.log(currentTime);
+    console.log(typeof(currentTime));
+    console.log(currentEL.innerText)
 }
 
 setInterval(getTime, 1000)
